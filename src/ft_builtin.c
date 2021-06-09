8935e4af7bc73b7d6ab528de7308d8e4cf12e16c
Pinkie Gurn <pgurn@va-c5.kzn.21-school.ru>
2021-06-09 17:31:34 +0300

    Trying builtin fn

--- src/ft_builtin.c
+++ src/ft_builtin.c
@@ -3,6 +3,19 @@
 int		ft_echo(void)
 {
 	printf("in_echo\n");
+	
+	return (1);
+}
+
+int		ft_cd(void)
+{
+	printf("in_cd\n");
+	return (1);
+}
+
+int		ft_pwd(void)
+{
+	printf("in_pwd\n");
 	return (1);
 }
 
@@ -10,5 +23,9 @@ int		ft_fn_selector(void)
 {
 	if (!ft_strncmp(shell.cmd_table->token[0], "echo", 5))
 		return (ft_echo());
+	if (!ft_strncmp(shell.cmd_table->token[0], "cd", 2))
+		return (ft_cd());
+	if (!ft_strncmp(shell.cmd_table->token[0], "pwd", 3))
+		return (ft_pwd());
 	return (0);
 }
