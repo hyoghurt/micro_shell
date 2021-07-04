7bcadb48ab3d30161c529c1baadc71ff1f91d7b3
Hunter Yoghurt <hyoghurt@student.21-school.ru>
2021-07-04 22:34:13 +0300

    build cd correct

--- src/ft_bl_cd.c
+++ src/ft_bl_cd.c
@@ -1,15 +1,76 @@
 #include "minishell.h"
 
+void	ft_cd_one(void);
+void	ft_update_oldpwd(void);
+void	ft_update_pwd(void);
+
+int	ft_cd(char **cmd)
+{
+	if (cmd[1] == 0)
+		ft_cd_one();
+	else if (cmd[2])
+	{
+		ft_putstr_fd("minishell: cd: too many arguments\n", 2);
+		g_shell.status = 1;
+	}
+	else if (chdir(cmd[1]) == -1)
+	{
+		ft_print_string("minishell", cmd[1], strerror(errno));
+		g_shell.status = 1;
+	}
+	else
+	{
+		ft_update_oldpwd();
+		ft_update_pwd();
+		g_shell.status = 0;
+	}
+	return (1);
+}
+
+void	ft_cd_one(void)
+{
+	char	*path;
+
+	path = ft_getset("HOME");
+	if (*path == 0)
+	{
+		free(path);
+		ft_putstr_fd("minishell: cd: no env HOME\n", 2);
+		g_shell.status = 1;
+	}
+	else if (chdir(path) == -1)
+	{
+		ft_print_string("minishell", path, strerror(errno));
+		free(path);
+		g_shell.status = 1;
+	}
+	else
+	{
+		free(path);
+		ft_update_oldpwd();
+		ft_update_pwd();
+		g_shell.status = 0;
+	}
+}
+
 void	ft_update_oldpwd(void)
 {
-	char	*string;
+	char	*pwd;
 	char	*full_oldpwd;
 	int		i;
 
 	i = ft_var_is_present("OLDPWD=");
-	string = ft_getset("PWD");
-	full_oldpwd = ft_strjoin("OLDPWD=", string);
-	ft_swap_var_val(i, full_oldpwd);
+	pwd = ft_getset("PWD");
+	if (!pwd)
+		ft_exit("minishell: update_oldpwd: malloc: ", "error");
+	full_oldpwd = ft_strjoin("OLDPWD=", pwd);
+	free(pwd);
+	if (!full_oldpwd)
+		ft_exit("minishell: update_oldpwd: malloc: ", "error");
+	if (i >= 0)
+		ft_swap_var_val(i, full_oldpwd);
+	//else
+	//write export full_oldpwd
 }
 
 void	ft_update_pwd(void)
@@ -18,34 +79,16 @@ void	ft_update_pwd(void)
 	char	*full_pwd;
 	int		i;
 
-	pwd = getcwd(NULL, 0);
 	i = ft_var_is_present("PWD=");
+	pwd = getcwd(NULL, 0);
+	if (!pwd)
+		ft_exit("minishell: update_oldpwd: malloc: ", "error");
 	full_pwd = ft_strjoin("PWD=", pwd);
 	free(pwd);
-	ft_swap_var_val(i, full_pwd);
-}
-
-int	ft_cd(char **cmd)
-{
-	char	*path;
-	int		ret;
-
-	g_shell.status = 0;
-	if (cmd && cmd[1] && cmd[2])
-	{
-		ft_putstr_fd("minishell: cd: too many arguments\n", 2);
-		g_shell.status = 1;
-		return (1);
-	}
-	path = cmd[1];
-	ret = chdir(path);
-	if (ret == -1)
-	{
-		ft_print_string("minishell", path, strerror(errno));
-		g_shell.status = 1;
-		return (1);
-	}
-	ft_update_oldpwd();
-	ft_update_pwd();
-	return (1);
+	if (!full_pwd)
+		ft_exit("minishell: update_pwd: malloc: ", "error");
+	if (i >= 0)
+		ft_swap_var_val(i, full_pwd);
+	//else
+	//write export full_pwd
 }
